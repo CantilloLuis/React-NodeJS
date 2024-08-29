a43f107dda1bb27c578836bee8aa52a2195cd862
CantilloLuis <cantilloluiseduardo52@gmail.com>
2024-08-29 09:57:21 -0500

    agregando axios al proyecto para consumir los datos de alguna api

--- src/principal/vistaPrincipa.js
+++ src/principal/vistaPrincipa.js
@@ -1,108 +1,126 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 
 
-function vistaPrincipal() {
+function VistaPrincipal() {
 
     const [getAuto, setAuto] = useState([]);
+
+    useEffect(() => {
+        axios.get('')
+
+            .then(response => {
+                setAuto(response.data.items);
+                console.log(response.data.items);
+            })
+            .catch(error => {
+            });
+    }, []);
 
     return (
         <>
             <br></br>
-            <p class="text-3xl text-gray-900 dark:text-white text-center">Tabla</p>
+            <p className="text-3xl text-gray-900 dark:text-white text-center">Tabla</p>
             <br></br>
 
 
-            <div class=" w-3/4 mx-auto relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+            <div className=" w-3/4 mx-auto relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                            <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
                                 Product name
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Color
                             </th>
-                            <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                            <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
                                 Category
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" className="px-6 py-3">
                                 Price
                             </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr class="border-b border-gray-200 dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        <tr className="border-b border-gray-200 dark:border-gray-700">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
                                 Apple MacBook Pro 17"
                             </th>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 Silver
                             </td>
-                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
                                 Laptop
                             </td>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 $2999
                             </td>
                         </tr>
-                        <tr class="border-b border-gray-200 dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        <tr className="border-b border-gray-200 dark:border-gray-700">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
                                 Microsoft Surface Pro
                             </th>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 White
                             </td>
-                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
                                 Laptop PC
                             </td>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 $1999
                             </td>
                         </tr>
-                        <tr class="border-b border-gray-200 dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        <tr className="border-b border-gray-200 dark:border-gray-700">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
                                 Magic Mouse 2
                             </th>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 Black
                             </td>
-                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
                                 Accessories
                             </td>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 $99
                             </td>
                         </tr>
-                        <tr class="border-b border-gray-200 dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        <tr className="border-b border-gray-200 dark:border-gray-700">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
                                 Google Pixel Phone
                             </th>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 Gray
                             </td>
-                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
                                 Phone
                             </td>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 $799
                             </td>
                         </tr>
                         <tr>
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
                                 Apple Watch 5
                             </th>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 Red
                             </td>
-                            <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
                                 Wearables
                             </td>
-                            <td class="px-6 py-4">
+                            <td className="px-6 py-4">
                                 $999
                             </td>
                         </tr>
                     </tbody>
                 </table>
+            </div>
+
+            <div>
+                {getAuto.map(items => (
+                    <p key={items.id}>{items.name}</p>
+                ))}
             </div>
 
 
@@ -114,4 +132,4 @@ function vistaPrincipal() {
 
 }
 
-export default vistaPrincipal;
+export default VistaPrincipal;
