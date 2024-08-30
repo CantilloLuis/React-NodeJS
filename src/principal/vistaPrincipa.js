fa68b0eae26c8530edd879aba88c50ef2feac7e8
CantilloLuis <cantilloluiseduardo52@gmail.com>
2024-08-30 13:22:22 -0500

    Agregando los nombres de columnas correspondiente a los datos que se traeran del backend

--- src/principal/vistaPrincipa.js
+++ src/principal/vistaPrincipa.js
@@ -20,7 +20,7 @@ function VistaPrincipal() {
     return (
         <>
             <br></br>
-            <p className="text-3xl text-gray-900 dark:text-white text-center">Tabla</p>
+            <p className="text-3xl text-gray-900 dark:text-white text-center">Tabla de autos</p>
             <br></br>
 
 
@@ -29,16 +29,16 @@ function VistaPrincipal() {
                     <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
                         <tr>
                             <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-                                Product name
+                                Id
                             </th>
                             <th scope="col" className="px-6 py-3">
-                                Color
+                                Nombre
                             </th>
                             <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-                                Category
+                                cilindraje
                             </th>
                             <th scope="col" className="px-6 py-3">
-                                Price
+                                kilometraje
                             </th>
                         </tr>
                     </thead>
@@ -57,70 +57,8 @@ function VistaPrincipal() {
                                 $2999
                             </td>
                         </tr>
-                        <tr className="border-b border-gray-200 dark:border-gray-700">
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                                Microsoft Surface Pro
-                            </th>
-                            <td className="px-6 py-4">
-                                White
-                            </td>
-                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                Laptop PC
-                            </td>
-                            <td className="px-6 py-4">
-                                $1999
-                            </td>
-                        </tr>
-                        <tr className="border-b border-gray-200 dark:border-gray-700">
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                                Magic Mouse 2
-                            </th>
-                            <td className="px-6 py-4">
-                                Black
-                            </td>
-                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                Accessories
-                            </td>
-                            <td className="px-6 py-4">
-                                $99
-                            </td>
-                        </tr>
-                        <tr className="border-b border-gray-200 dark:border-gray-700">
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                                Google Pixel Phone
-                            </th>
-                            <td className="px-6 py-4">
-                                Gray
-                            </td>
-                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                Phone
-                            </td>
-                            <td className="px-6 py-4">
-                                $799
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
-                                Apple Watch 5
-                            </th>
-                            <td className="px-6 py-4">
-                                Red
-                            </td>
-                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                Wearables
-                            </td>
-                            <td className="px-6 py-4">
-                                $999
-                            </td>
-                        </tr>
                     </tbody>
                 </table>
-            </div>
-
-            <div>
-                {getAuto.map(items => (
-                    <p key={items.id}>{items.name}</p>
-                ))}
             </div>
 
 
