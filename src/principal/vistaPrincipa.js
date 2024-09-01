1d61fa89344144fec2b7553df2b0279975373646
CantilloLuis <cantilloluiseduardo52@gmail.com>
2024-09-01 16:44:11 -0500

    Eliminando algunas filas de la tabla

--- src/principal/vistaPrincipa.js
+++ src/principal/vistaPrincipa.js
@@ -47,15 +47,6 @@ function VistaPrincipal() {
                             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
                                 Apple MacBook Pro 17"
                             </th>
-                            <td className="px-6 py-4">
-                                Silver
-                            </td>
-                            <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">
-                                Laptop
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
                         </tr>
                     </tbody>
                 </table>
